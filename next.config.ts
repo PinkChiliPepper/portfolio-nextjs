fe2f22e18ef60cc198b670ab47643f9fb8018946
Jessie <jessievandenakker@hotmail.com>
2025-07-14 20:37:09 +0200

    configure pages: update next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  basePath: "/portfolio",
+  basePath: "/portfolio-nextjs",
   output: "export",
   reactStrictMode: true,
 };
