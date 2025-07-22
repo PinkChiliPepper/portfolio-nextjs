7b6299066175fe0a6681ea608c3df26ca2d31112
Jessie <jessievandenakker@hotmail.com>
2025-07-22 16:51:46 +0200

    image: play around with styling

--- src/components/images/RoundImage/RoundImage.tsx
+++ src/components/images/RoundImage/RoundImage.tsx
@@ -2,13 +2,13 @@ import Image from "@/components/images/Image"
 import { ImageProps } from "@/components/images/Image/Image"
 import { FunctionComponent } from "react"
 
-interface Props extends ImageProps { }
-
-const RoundImage: FunctionComponent<Props> = ({ ...rest }) => {
+const RoundImage: FunctionComponent<ImageProps> = (props) => {
   return (
     <div className='rounded-full border-2 border-tertiary-text'>
       <div className='rounded-full border-2 border-secondary-text translate-x-5 translate-y-2.5'>
-        <Image {...rest} className="rounded-full" />
+        <div className='rounded-full border-2 border-primary-text-light translate-x-6 translate-y-3'>
+          <Image {...props} className="rounded-full border-5 border-tertiary-text" />
+        </div>
       </div>
     </div>
   )
